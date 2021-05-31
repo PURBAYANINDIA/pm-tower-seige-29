32b69178fb2f4a2b1a90d49e16f8db939b8e66db
PURBAYANINDIA <74766371+PURBAYANINDIA@users.noreply.github.com>
2021-05-31 15:03:22 +0530

    Add files via upload

---
+++ project 29/Slingshot.js
@@ -0,0 +1,32 @@
+class Slingshot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 5
+        }
+    //   this.sling1 = loadImage
+
+
+
+        this.pointB = pointB
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+
+    fly(){
+        this.sling.bodyA = null;
+    }
+
+    display(){
+        if(this.sling.bodyA){
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+            strokeWeight(4);
+            stroke("green");
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+    }
+    
+}
